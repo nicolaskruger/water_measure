be22532d3f8771cd5f3dff96b961338eecad4f7d
nkruger <nicolas.nkruger@gmail.com>
2024-08-27 17:34:01 -0300

    feat: :sparkles: poc post

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,9 +1,22 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, HttpStatus, Post, Res } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Response } from 'express';
+
+type WaterInfo = {
+  image: string;
+  customer_code: string;
+  measure_datetime: string;
+  mesure_type: 'WATER' | 'GAS';
+};
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
+
+  @Post()
+  async upload(@Body() waterInfo: WaterInfo, @Res() res: Response) {
+    res.status(HttpStatus.OK).json(waterInfo);
+  }
 
   @Get()
   getHello(): string {
